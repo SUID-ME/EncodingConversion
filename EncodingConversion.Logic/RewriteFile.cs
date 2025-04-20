e427906a86fd44b71c93b9a6c4c368e4de0bc37b
SUID-ME <86769839+SUID-ME@users.noreply.github.com>
2025-04-20 05:29:04 +0300

    Add encode check and autoselect

--- EncodingConversion.Logic/RewriteFile.cs
+++ EncodingConversion.Logic/RewriteFile.cs
@@ -8,6 +8,7 @@ namespace EncodingConversion.Logic
     internal class RewriteFile
     {
         private Encoding _encFrom;
+        private Encoding _encTo = Encoding.UTF8;
 
         internal RewriteFile(Encoding encoderFrom)
         {
@@ -22,12 +23,19 @@ namespace EncodingConversion.Logic
 
         public bool Rewrite(string filepath)
         {
+            Encoding encoding = _encFrom;
+
+            if (CheckEncoding(filepath, ref encoding) == false)
+            {
+                return false;
+            }
+
             string? line;
             string decodeFilePath = filepath + ".decode";
             try
             {
                 StreamWriter sw = new StreamWriter(decodeFilePath);
-                StreamReader sr = new StreamReader(filepath, _encFrom);
+                StreamReader sr = new StreamReader(filepath, encoding);
                 line = sr.ReadLine();
                 while (line != null)
                 {
@@ -45,12 +53,50 @@ namespace EncodingConversion.Logic
             }
             catch (Exception e)
             {
-                //MessageBox.Show("Exception: " + e.Message, "Error!");
+                Debug.WriteLine("Exception: " + e.Message, "Error!");
                 return false;
             }
 
 
             return true;
+        }
+
+        private bool CheckEncoding(string filePath, ref Encoding encoding)
+        {
+            using (FileStream fileStream = File.OpenRead(filePath))
+            {
+                Ude.CharsetDetector charsetDetector = new Ude.CharsetDetector();
+                charsetDetector.Feed(fileStream);
+                charsetDetector.DataEnd();
+
+                if (charsetDetector.Charset != null)
+                {
+                    Debug.WriteLine($"Кодировка - {charsetDetector.Charset}, Уверенность в результате - {charsetDetector.Confidence}.");
+
+                    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                    Encoding enc = Encoding.GetEncoding(charsetDetector.Charset);
+
+                    if (enc == _encTo)
+                    {
+                        Debug.WriteLine($"Файл '{filePath}' Уже в нужной кодировке");
+                        return false;
+                    }
+                    else if (enc != _encFrom)
+                    {
+                        Debug.WriteLine($"Файл '{filePath}' не соответствует указанной начальной кодировке");
+                        encoding = enc;
+                        return true;
+                    } else
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    Debug.WriteLine("Проблема с определением кодировки.");
+                    return false;
+                }
+            }
         }
     }
 }
